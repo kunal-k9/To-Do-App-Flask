5e08603f019476056448c30aee031f7517f8dbec
kunal-k9 <157028248+kunal-k9@users.noreply.github.com>
2024-02-22 12:10:46 +0530

    Added search functionality

--- app.py
+++ app.py
@@ -7,51 +7,63 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-app.app_context().push()
+
+fdt = datetime.utcnow().replace(microsecond=0)
 
 class Todo(db.Model):
     sno = db.Column(db.Integer,primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_created = db.Column(db.DateTime, default=fdt)
 
     def __repr__(self):
         return f"{self.sno} - {self.title}"
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    with app.app_context():
-        if request.method == 'POST':
-            title = request.form['title']
-            desc = request.form['desc']
-            todo = Todo(title=title, desc=desc)
-            db.session.add(todo)
-            db.session.commit()
-        todos = Todo.query.all()
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+    todos = Todo.query.all()
     return render_template('index.html', todos=todos)
 
 @app.route('/update/<int:sno>', methods=['GET', 'POST'])
 def update(sno):
-    with app.app_context():
-        if request.method == 'POST':
-            title = request.form['title']
-            desc = request.form['desc']
-            todo = Todo.query.filter_by(sno=sno).first()
-            todo.title = title
-            todo.desc = desc
-            db.session.add(todo)
-            db.session.commit()
-            return redirect('/')
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
     todo = Todo.query.filter_by(sno=sno).first()
     return render_template('update.html', todo=todo)
 
 @app.route('/delete/<int:sno>')
 def delete(sno):
-    with app.app_context():
-        todo = Todo.query.filter_by(sno=sno).first()
-        db.session.delete(todo)
-        db.session.commit()
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
     return redirect('/')
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+            query = request.form['search']
+            todo_title = Todo.query.filter(Todo.title.ilike(f'%{query}%')).all()
+            todo_desc = Todo.query.filter(Todo.desc.ilike(f'%{query}%')).all()
+            todos = todo_title + todo_desc
+            print(todos)
+    return render_template('search.html', todos=todos)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
